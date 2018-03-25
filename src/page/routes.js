5d818504569750d88f132277bef130b3d44bb29f
Patrick van Bergen <patrick.vanbergen@gmail.com>
2018-03-25 10:37:59 +0200

    Route page urls to root

--- src/page/routes.js
+++ src/page/routes.js
@@ -1,3 +1,7 @@
+// react-router-dom
+// https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/philosophy.md
+
+// The .htaccess file takes care that /kattebel/add is handled by the script on /kattebel
 
 export function getRoute(path)
 {
